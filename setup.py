a7c4f29e618f9fc4a7fa6a4fc83a0d1e0647ab1e
Anthony Rebello <rebello.anthony@gmail.com>
2017-04-12 00:01:07 +0530

    added travis integration

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
 from setuptools import setup, find_packages
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+def get_long_description():
+    dirs = [ HERE ]
+    if os.getenv("TRAVIS"):
+        dirs.append(os.getenv("TRAVIS_BUILD_DIR"))
+
+    long_description = ""
+
+    for d in dirs:
+        rst_readme = os.path.join(d, "README.rst")
+        if not os.path.exists(rst_readme):
+            continue
+
+        with open(rst_readme) as fp:
+            long_description = fp.read()
+            return long_description
+
+    return long_description
+
+long_description = get_long_description()
 
 version = '0.0.1'
 setup(
     name="tee",
     version=version,
     description="Python library to tee stderr/stdout temporarily",
-    long_description="Python library to tee stderr/stdout temporarily",
+    long_description=long_description,
     keywords='tee',
     author='Anthony Rebello',
     author_email="rebello.anthony@gmail.com",
